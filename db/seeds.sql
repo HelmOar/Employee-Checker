6cd1d0b35c3327ed2a135813b3f7227223658826
caroline <chelmore1@bigpond.com>
2023-05-05 14:05:19 +1000

    seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,38 +1,38 @@
-INSERT INTO department (department_id, department_name)
+INSERT INTO department (department_name)
 VALUES
-(1, 'Engineering'),
-(2, 'Sales'),
-(3, 'Finance'),
-(4, 'Legal'),
-(5, 'HR'),
-(6, 'Marketing'),
-(7, 'Customer Seervice'),
-(8, 'Administration');	
+('Engineering'),
+('Sales'),
+('Finance'),
+('Legal'),
+('HR'),
+('Marketing'),
+('Customer Seervice'),
+('Administration');	
 
 
 
 
-INSERT INTO roles (role_id, title, salary, department_id) 
+INSERT INTO roles ( title, salary, department_id) 
 VALUES 
-(001,'Software Engineer', 100000, 1),
-(002, 'Account Manager', 130000, 2),
-(003,'Accountant', 160000, 3),
-(004,'Legal Team Lead', 250000, 4),
-(005,'Human Resources Manager', 190000, 5)
-(006,'Marketing Manager', 180000, 6),
-(007,'Customer Service Manager', 160000, 7),
-(008,'Administration Manager', 150000, 8);
+('Software Engineer', 100000, 1),
+('Account Manager', 130000, 2),
+('Accountant', 160000, 3),
+('Legal Team Lead', 250000, 4),
+('Human Resources Manager', 190000, 5)
+('Marketing Manager', 180000, 6),
+('Customer Service Manager', 160000, 7),
+('Administration Manager', 150000, 8);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-('Emma', 'Thompson', 001, 101 ),
-('Benjamin', 'Wilson'002, 103),
-('Olivia', 'Parker'003,104 ),
-('Jack', 'Edwards'004, 105 ),
-('Chloe', 'Green' 005, 106),
-('Nathan', 'Patel'006, 109),
-('Leah', 'Anderson'007, 109),
-('William', 'Brown'008, 109),
+('Emma', 'Thompson', 1, 1 ),
+('Benjamin', 'Wilson'2, 2),
+('Olivia', 'Parker'3, 3 ),
+('Jack', 'Edwards' 4, 4 ),
+('Chloe', 'Green' 5, 5),
+('Nathan', 'Patel'6, 6),
+('Leah', 'Anderson'7, 7),
+('William', 'Brown'8, 8),
 --('Megan', 'Ross'),
 --('Ethan', 'Reynolds');
 
